0393e1ac2da2a0bd3939fce07ebf5cf027adc083
stevenloppe <hatchways@loppe.ca>
2022-04-30 14:54:49 -0700

    Fix sent messages not being immediately displayed
    When a user sent a message, it would not immediately appear on the screen. This was happening both for new conversations and for existing conversations.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -62,9 +62,9 @@ const Home = ({ user, logout }) => {
     });
   };
 
-  const postMessage = (body) => {
+  const postMessage = async (body) => {
     try {
-      const data = saveMessage(body);
+      const data = await saveMessage(body);
 
       if (!body.conversationId) {
         addNewConvo(body.recipientId, data.message);
@@ -87,7 +87,7 @@ const Home = ({ user, logout }) => {
           convo.id = message.conversationId;
         }
       });
-      setConversations(conversations);
+      setConversations([...conversations]);
     },
     [setConversations, conversations]
   );
@@ -112,7 +112,7 @@ const Home = ({ user, logout }) => {
           convo.latestMessageText = message.text;
         }
       });
-      setConversations(conversations);
+      setConversations([...conversations]);
     },
     [setConversations, conversations]
   );
