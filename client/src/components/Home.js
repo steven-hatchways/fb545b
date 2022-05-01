da2d22d56cd360564142b3356869c993c564ca1c
stevenloppe <hatchways@loppe.ca>
2022-05-01 11:58:31 -0700

    A copy of messages is needed before modifying it
    When modifying the `.messages` on a conversation object, a copy of the messages must also be made as well as making a copy of the conversation object itself.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -90,7 +90,7 @@ const Home = ({ user, logout }) => {
       setConversations((prev) =>
         prev.map((convo) => {
           if (convo.otherUser.id === recipientId) {
-            const convoCopy = { ...convo };
+            const convoCopy = { ...convo, messages: [ ...convo.messages ] };
             convoCopy.messages.push(message);
             convoCopy.latestMessageText = message.text;
             convoCopy.id = message.conversationId;
@@ -121,7 +121,7 @@ const Home = ({ user, logout }) => {
       setConversations((prev) =>
         prev.map((convo) => {
           if (convo.id === message.conversationId) {
-            const convoCopy = { ...convo };
+            const convoCopy = { ...convo, message: [...convo.messages] };
             convoCopy.messages.push(message);
             convoCopy.latestMessageText = message.text;
             return convoCopy;
