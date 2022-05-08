c17615e670ff466aea50339cadb9b408d86af6b1
stevenloppe <hatchways@loppe.ca>
2022-05-08 12:24:47 -0700

    Remove unnecessary dependency from addNewConvo
    Remove the unnecessary dependency `conversations` from the addNewConvo's useCallback dependencies.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -178,7 +178,7 @@ const Home = ({ user, logout }) => {
         })
       );
     },
-    [setConversations, conversations]
+    [setConversations]
   );
 
   const addMessageToConversation = useCallback(
