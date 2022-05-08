67a8f8d4391237a0c22a41ebb988cdaec94197d7
stevenloppe <hatchways@loppe.ca>
2022-05-08 12:42:39 -0700

    Make preview text bold if chat has unread messages

--- client/src/components/Sidebar/Chat.js
+++ client/src/components/Sidebar/Chat.js
@@ -45,7 +45,7 @@ const Chat = ({ conversation, setActiveChat }) => {
         online={otherUser.online}
         sidebar={true}
       />
-      <ChatContent conversation={conversation} />
+      <ChatContent conversation={conversation} hasUnreadMessages={countUnreadMessages() > 0} />
       <Badge
           badgeContent={countUnreadMessages()}
           color="primary"
